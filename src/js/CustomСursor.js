090f427b3212b2462bdeae61e865409c0420fe09
Vadim Roziznan <roziznan_v@mail.ru>
2024-03-31 21:32:36 +0300

    in progress

--- src/js/CustomСursor.js
+++ src/js/CustomСursor.js
@@ -1,21 +1,29 @@
 export default class CustomСursor {
   constructor(element) {
     this.element = element;
+    this.clickCountHit = 0;
+    this.clickCountMissed = 0;
   }
   changeCursor() {
-    /*const field = document.querySelector('.field')*/
-    this.element.style.cursor = 'url("../images/hammer.png"), auto';
+    this.element.style.cursor = 'url("../images/hammer_up.png"), auto';
   }
   rotateCursor() {
-    this.element.addEventListener('mousedown', event => {
+    this.element.addEventListener("mousedown", (event) => {
       this.element.style.cursor = 'url("../images/hammer_down.png"), auto';
-      if (event.target.tagName === 'IMG') {
-        this.mouseEvent();
+      if (event.target.tagName === "IMG") {
+        this.clickCountHit += 1;
+        event.target.remove();
+      } else {
+        this.clickCountMissed += 1;
       }
     });
-    
-    this.element.addEventListener('mouseup', event => {
+
+    this.element.addEventListener("mouseup", () => {
       this.element.style.cursor = 'url("../images/hammer_up.png"), auto';
     });
+  }
+  resetCounter() {
+    this.clickCountHit = 0;
+    this.clickCountMissed = 0;
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -4,18 +4,41 @@ import AddImage from "./AddImage";
 import CustomСursor from "./CustomСursor";
 
 document.addEventListener("DOMContentLoaded", () => {
-  new Field(document.querySelector(".field"), 4, 4).fieldGenerator();
+  const score = document.querySelector(".score");
+  const life = document.querySelector(".life");
+  const buttonNewGame = document.querySelector(".new-game");
+  const field = new Field(document.querySelector(".field"), 4, 4);
+  field.fieldGenerator();
+
   const newImage = new AddImage(document.querySelector(".field"));
   const move = new Moving();
 
-  const cursor = new CustomСursor(document.querySelector('.field'))
-  cursor.changeCursor()
-  cursor.rotateCursor()
+  const cursor = new CustomСursor(document.querySelector(".field"));
+  cursor.changeCursor();
+  cursor.rotateCursor();
 
+  let game = false;
 
-
-  setInterval(() => {
-    const step = move.randomStep(4, 4);
-    newImage.createImage(step.width, step.height);
-  }, 1000);
+  buttonNewGame.addEventListener("click", () => {
+    cursor.resetCounter();
+    if (!game) {
+      const interval = setInterval(() => {
+        game = true;
+        score.textContent = cursor.clickCountHit;
+        life.textContent = cursor.clickCountMissed;
+        if (cursor.clickCountHit === 5) {
+          clearInterval(interval);
+          game = false;
+          alert("Вы выиграли!");
+        }
+        if (cursor.clickCountMissed === 5) {
+          clearInterval(interval);
+          game = false;
+          alert("Вы проиграли!");
+        }
+        const step = move.randomStep(4, 4);
+        newImage.createImage(step.width, step.height);
+      }, 1000);
+    }
+  });
 });
